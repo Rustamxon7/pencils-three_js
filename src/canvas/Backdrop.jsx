4654ffed4f94c4ce3ce9ce44b596955d2f2dfc2f
rustamtolipov <rustamxontolipov788@gmail.com>
2023-04-10 05:58:13 +0500

    🔒️

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import { easing } from 'maath';
 import { useFrame } from '@react-three/fiber';
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
-import { useControls } from 'leva';
 
 const Backdrop = () => {
   const shadows = useRef();
